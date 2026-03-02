89973ad181132991188ba3bd00ed3e9c8c288da9
shakilla1 <s.uwamahoro1@alustudent.com>
2026-03-02 06:18:24 -0800

    add everything

--- models/state.py
+++ models/state.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
+
 import models
 from models.base_model import BaseModel, Base
-import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class"""
 
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """ Returns the list of City instances with state_id
-            equals to the current State.id. """
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+        # DBStorage: define relationship
+        cities = relationship(
+            'City',
+            backref="state",
+            cascade="all, delete, delete-orphan"
+        )
+
+    @property
+    def cities(self):
+        """
+        FileStorage: return list of City instances with state_id equal to
+        the current State.id
+        """
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            return []  # DBStorage uses relationship
+        city_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
