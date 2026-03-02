f67c7ad7aad0ac5d409dd370cd857e86c38df3af
shakilla1 <s.uwamahoro1@alustudent.com>
2026-03-02 06:03:49 -0800

    add change

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,86 +1,84 @@
 #!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
+""" new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-from models.user import User
 from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
 
-classes = {
-    "Amenity": Amenity,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User
-}
-
 
 class DBStorage:
-    """Interacts with the MySQL database"""
-
+    """ create tables in environmental"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initialize the DBStorage"""
-        user = getenv('HBNB_MYSQL_USER')
-        pwd = getenv('HBNB_MYSQL_PWD')
-        host = getenv('HBNB_MYSQL_HOST')
-        db = getenv('HBNB_MYSQL_DB')
-        env = getenv('HBNB_ENV')
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        self.__engine = create_engine(
-            f"mysql+mysqldb://{user}:{pwd}@{host}/{db}",
-            pool_pre_ping=True
-        )
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
 
         if env == "test":
             Base.metadata.drop_all(self.__engine)
-        self.reload()
 
     def all(self, cls=None):
-        """Query all objects"""
-        new_dict = {}
-        for clss in classes.values():
-            if cls is None or cls == clss or cls == clss.__name__:
-                objs = self.__session.query(clss).all()
-                for obj in objs:
-                    key = f"{obj.__class__.__name__}.{obj.id}"
-                    new_dict[key] = obj
-        return new_dict
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if isinstance(cls, str):
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """Add object to current session"""
-        if obj is not None:
-            self.__session.add(obj)
+        """add a new element in the table
+        """
+        self.__session.add(obj)
 
     def save(self):
-        """Commit changes to the database"""
+        """save changes
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete object from current session"""
-        if obj is not None:
-            self.__session.delete(obj)
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
 
     def reload(self):
-        """Create all tables and initialize session"""
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine,
-            expire_on_commit=False
-        )
-        self.__session = scoped_session(session_factory)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
 
     def close(self):
-        """Remove the current session and create a fresh one"""
-        self.__session.remove()
-        self.reload()
+        """ calls remove()
+        """
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
-"""Holds class State"""
-
+""" State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """Representation of a State"""
+    """ State class """
 
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            "City",
-            backref="state",
-            cascade="all, delete, delete-orphan"
-        )
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes State"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def cities(self):
-        """Return list of City objects linked to this State (FileStorage only)"""
-        if getenv('HBNB_TYPE_STORAGE') == 'db':
-            return []
-        all_cities = models.storage.all(City)
-        return [city for city in all_cities.values() if city.state_id == self.id]
+        @property
+        def cities(self):
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
